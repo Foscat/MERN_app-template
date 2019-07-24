6a9f4611c6f395e78cfe970aa9f712ddbf03365c
k-fizzle <foscat4o1k@outlook.com>
2019-07-24 16:07:25 -0500

    made folder for models

--- client/src/components/pages/Home/index.js
+++ client/src/components/pages/Home/index.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import { Container, Row, Col, Button ,FormGroup, Label, Input } from 'reactstrap';
+import { Container, Row, Col, Button  } from 'reactstrap';
 import API from '../../../utils/API';
 import TextCard from '../../parts/TextCard';
 import CustomerSignUp from '../../parts/CustomerSignUp';
 import SweetAlert from "react-bootstrap-sweetalert";
+import EditUser from '../../parts/Models/EditUser';
 
 export class Home extends Component{
     constructor(props){
@@ -96,7 +97,7 @@ export class Home extends Component{
     }
 
     // Sweet alert model that contains form for PUT operations 
-    contactModal = user => {
+    editUserModal = user => {
 
         this.setState({ 
             updateFirstName: user.first_name,
@@ -108,49 +109,11 @@ export class Home extends Component{
         
         let text = (
           <div>
-            Phone number: {user.phone_num}
-            <br />
-            <form
-              className="m-2"
-              action="#" //This does not need a action since submit function handles info flow
-              encType="text/plain"
-              method="put"
-              id="update-form"
-            >
-
-                <FormGroup className="form-group">
-                    <Label for="updateFirstName">First Name</Label>
-                    <Input type="text" name="updateFirstName" onChange={this.handleInputChange}
-                    id="updateFirstName" defaultValue={user.first_name} placeholder="Enter first name"/>
-                </FormGroup>
-
-                <FormGroup className="form-group">
-                    <Label for="updateLastName">Last Name</Label>
-                    <Input type="text" name="updateLastName" onChange={this.handleInputChange}
-                    id="updateLastName" defaultValue={user.last_name} placeholder="Enter last name"/>
-                </FormGroup>
-
-                <FormGroup className="form-group">
-                    <Label for="updateEmail">Preferred Email</Label>
-                    <Input type="email" name="updateEmail" onChange={this.handleInputChange}
-                    id="updateEmail" defaultValue={user.email} placeholder="Enter email"/>
-                </FormGroup>
-
-                <FormGroup className="form-group">
-                    <Label for="updatePassword">Password</Label>
-                    <Input type="password" name="updatePassword" onChange={this.handleInputChange}
-                    id="updatePassword" defaultValue={user.password} placeholder="Enter password"/>
-                </FormGroup>
-
-                <FormGroup className="form-group">
-                    <Label for="updatePhoneNum">Phone Number</Label>
-                    <Input type="number" name="updatePhoneNum" onChange={this.handleInputChange}
-                    id="updatePhoneNum" defaultValue={user.phone_num} placeholder="Enter phone number"/>
-                </FormGroup>
-
-                <Button className="btn btn-success" onClick={() => this.handleUpdateFormSubmit(user._id) }>Submit</Button>
-
-            </form>
+            <EditUser 
+                handleInputChange={this.handleInputChange}
+                handleUpdateFormSubmit={this.handleUpdateFormSubmit}
+                user={user}
+            />
           </div>
         )
         // Update state to show model
@@ -253,7 +216,7 @@ export class Home extends Component{
                                                     Delete
                                                 </Button>
                                                 {/* Edit user button */}
-                                                <Button onClick={() =>  this.contactModal(user)}>
+                                                <Button onClick={() =>  this.editUserModal(user)}>
                                                     Edit
                                                 </Button>
                                             </TextCard>
