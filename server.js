ea498296559c35c97652968b3b78de9968ee54c7
k-fizzle <foscat4o1k@outlook.com>
2020-01-20 11:10:05 -0600

    move headers for cors

--- server.js
+++ server.js
@@ -15,6 +15,13 @@ dotenv.config();
 app.use(express.urlencoded({ extended:true }));
 app.use(express.json());
 
+// Set express headers before creating routes to prevent cors issues
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 // if else statement stableizes deployment build to see pages and use backend routes.
 if (process.env.NODE_ENV === "production") {
